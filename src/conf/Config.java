fdce6fb5391a5c04409f761afe069ce454d527ab
LeeKyuSung <LeeKyuSung@DESKTOP-BIKHL2D>
2020-05-09 19:30:33 +0900

    change config

--- src/conf/Config.java
+++ src/conf/Config.java
@@ -1,8 +1,8 @@
 package conf;
 
 public class Config {
-	public static final String DB_HOST = "192.168.0.7";
-	public static final String DB_DBNAME = "TestDB";
+	public static final String DB_HOST = "211.231.80.215";
+	public static final String DB_DBNAME = "CTP471";
 	public static final String DB_USER_NAME = "ctp471";
 	public static final String DB_PASSWORD = "ctp471";
 	
