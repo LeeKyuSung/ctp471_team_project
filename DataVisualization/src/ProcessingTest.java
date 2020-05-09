c898610550bdc1baf9803b1a18225d01d000d9a6
LeeKyuSung <LeeKyuSung@DESKTOP-BIKHL2D>
2020-05-09 21:09:35 +0900

    add test class for processing

---
+++ DataVisualization/src/ProcessingTest.java
@@ -0,0 +1,25 @@
+import processing.core.PApplet;
+
+public class ProcessingTest extends PApplet {
+	 // The argument passed to main must match the class name
+    public static void main(String[] args) {
+        PApplet.main("ProcessingTest");
+    }
+
+    // method used only for setting the size of the window
+    public void settings(){
+    	size(400,400);   
+    }
+
+    // identical use to setup in Processing IDE except for size()
+    public void setup(){
+    	background(0);
+        stroke(255);
+        strokeWeight(10);
+    }
+
+    // identical use to draw in Prcessing IDE
+    public void draw(){
+    	line(0, 0, 500, 500);
+    }
+}
